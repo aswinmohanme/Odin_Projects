3dca1f1b8eb671fdd1fc4152be81ea3af00296f1
AswinMMohan <git.aswinmohan@gmail.com>
2016-09-06 06:25:42 +0530

    Create Input Exercise

---
+++ 03_Pine_Ruby_Exercises/02_Input.rb
@@ -0,0 +1,16 @@
+# @Author: Aswin Mohan <aswinmohan>
+# @Date:   2016-09-06T06:22:06+05:30
+# @Last modified by:   aswinmohan
+# @Last modified time: 2016-09-06T06:23:44+05:30
+
+# Write a program which asks for a person's first name, then middle, then last.
+# Finally, it should greet the person using their full name.
+firstName = gets.chomp
+middleName = gets.chomp
+lastName = gets.chomp
+
+puts 'Hello ' + firstName + ' ' + middleName + ' ' + lastName 
+
+# Write a program which asks for a person's favorite number. Have your program
+# add one to the number, then suggest the result as a bigger and better favorite
+# number. (Do be tactful about it, though.)
--- 03_Pine_Ruby_Exercises/numbers.rb
+++ 03_Pine_Ruby_Exercises/numbers.rb
@@ -1,7 +1,7 @@
 # @Author: Aswin Mohan <aswinmohan>
 # @Date:   2016-09-06T06:12:04+05:30
 # @Last modified by:   aswinmohan
-# @Last modified time: 2016-09-06T06:15:31+05:30
+# @Last modified time: 2016-09-06T06:21:40+05:30
 
 # How many hours are in a year
 puts 365 * 24
