a338de226215253c16fb1abac062773a5db10f3c
AswinMMohan <git.aswinmohan@gmail.com>
2016-09-06 06:18:38 +0530

    Complete Exercises in Numbers Section of Pine

---
+++ 03_Pine_Ruby_Exercises/numbers.rb
@@ -0,0 +1,13 @@
+# @Author: Aswin Mohan <aswinmohan>
+# @Date:   2016-09-06T06:12:04+05:30
+# @Last modified by:   aswinmohan
+# @Last modified time: 2016-09-06T06:15:31+05:30
+
+# How many hours are in a year
+puts 365 * 24
+
+# how many minutes in a decade
+puts 60 * 24 * 365 * 10
+
+# Age in Seconds
+puts 17.5 * 365 * 24 * 60 * 60
