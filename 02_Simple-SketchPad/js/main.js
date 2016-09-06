8796ad7acbc650c9304440776c6bc4729a257ea0
AswinMMohan <git.aswinmohan@gmail.com>
2016-09-06 06:20:27 +0530

    Cleared up

---
+++ 02_Simple-SketchPad/js/main.js
@@ -0,0 +1,21 @@
+/**
+* @Author: Aswin Mohan <aswinmohan>
+* @Date:   2016-09-03T21:50:17+05:30
+* @Last modified by:   aswinmohan
+* @Last modified time: 2016-09-03T22:49:26+05:30
+*/
+
+
+
+// The Main Javascript File
+
+var gridWidth = 16;
+
+$(document).ready(Function() {
+    displayGrid(gridWidth);
+});
+
+var displayGrid = function(width){
+    var size = 960;
+    var boxSize = (960 - 4 * n)*n;
+}
