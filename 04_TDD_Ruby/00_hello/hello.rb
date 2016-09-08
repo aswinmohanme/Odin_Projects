140105f8110aa618a00f3aca1b878bc0077c3efc
AswinMMohan <git.aswinmohan@gmail.com>
2016-09-08 23:41:29 +0530

    Finish Hello

---
+++ 04_TDD_Ruby/00_hello/hello.rb
@@ -0,0 +1,13 @@
+# @Author: Aswin Mohan <aswinmohan>
+# @Date:   2016-09-08T23:33:06+05:30
+# @Last modified by:   aswinmohan
+# @Last modified time: 2016-09-08T23:40:23+05:30
+
+def hello
+    return "Hello!"
+end
+
+# Greet Someone
+def greet(name)
+    return "Hello, #{name}!"
+end
