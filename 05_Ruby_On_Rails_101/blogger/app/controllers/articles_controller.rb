68cc43b511f1168e4c2258dd231a395325e8feb3
AswinMMohan <git.aswinmohan@gmail.com>
2016-09-25 11:54:13 +0530

    Add Delete Method

--- 05_Ruby_On_Rails_101/blogger/app/controllers/articles_controller.rb
+++ 05_Ruby_On_Rails_101/blogger/app/controllers/articles_controller.rb
@@ -1,16 +1,34 @@
 # @Author: Aswin Mohan <aswinmohan>
 # @Date:   2016-09-24T22:15:49+05:30
 # @Last modified by:   aswinmohan
-# @Last modified time: 2016-09-25T07:30:19+05:30
+# @Last modified time: 2016-09-25T09:37:53+05:30
 
 
 
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to articles_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path(@article)
     end
 end
--- 05_Ruby_On_Rails_101/blogger/app/helpers/articles_helper.rb
+++ 05_Ruby_On_Rails_101/blogger/app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
+# @Author: Aswin Mohan <aswinmohan>
+# @Date:   2016-09-24T22:11:42+05:30
+# @Last modified by:   aswinmohan
+# @Last modified time: 2016-09-25T09:38:20+05:30
+
+
+
 module ArticlesHelper
+    def article_params
+        params.require(:article).permit(:title , :body)
+    end
 end
