6ecbea22fe2d3a422a034096e3b3054db49e7ab4
AswinMMohan <git.aswinmohan@gmail.com>
2016-09-25 07:34:37 +0530

    Add show method to Articles and Drop Stylesheet

--- 05_Ruby_On_Rails_101/blogger/app/controllers/articles_controller.rb
+++ 05_Ruby_On_Rails_101/blogger/app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
+# @Author: Aswin Mohan <aswinmohan>
+# @Date:   2016-09-24T22:15:49+05:30
+# @Last modified by:   aswinmohan
+# @Last modified time: 2016-09-25T07:30:19+05:30
+
+
+
 class ArticlesController < ApplicationController
     def index
         @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 end
